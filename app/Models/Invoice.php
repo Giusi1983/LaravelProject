9d9f7fc8d84cadc5d1db1f81e70ce4e46c1d5213
Giusi1983 <68873947+Giusi1983@users.noreply.github.com>
2024-11-10 18:43:14 +0100

    added first files

---
+++ app/Models/Invoice.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    //
+    use HasFactory;
+    protected $fillable = ['client_id', 'invoice_number', 'invoice_date', 'total_amount'];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+}
